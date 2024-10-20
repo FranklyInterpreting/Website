5c0291b97da1daa23c6200cff03072c08b11ec69
FranklyInterpreting <frankly.dan@gmail.com>
2024-10-20 09:01:55 -0500

    java update

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-//create a javascript file that will display the url for my website as / or /resume
-const express = require('express');
-const app = express();
+//create a javascript file that will display the url for my website as / or /resume the java should return my homepage.html for / and my index.html for /resume
+const http = require('http');
+import * as fs from 'fs'
 
-app.get('/', (req, res) => {
-  res.send('/homepage.html');
+const server = http.createServer((req, res) => {
+  if (req.url === '/' || req.url === '/resume') {
+    const filePath = req.url === '/' ? 'homepage.html' : 'index.html';
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(404);
+        res.end('File not found');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404);
+    res.end('Page not found');
+  }
 });
 
-app.get('/resume', (req, res) => {
-  res.send('/index.html');
-});
-
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
 });
